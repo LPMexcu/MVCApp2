0d6fd386fc362bc1ec24a1b2ef39850a142ac91f
Marcos Lopez <71472051+LPMexcu@users.noreply.github.com>
2024-03-15 17:45:54 -0600

    Agregar archivos de proyecto.

---
+++ MVCApp2/Controllers/HelloWorldController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MVCApp2.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(string name, int numTimes =1)
+        {
+            ViewData["Message"] = "Hello "+ name ;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+        }
+        //Get: /HelloWorld/
+        //public string Index()
+        //{
+        //    return "This is my default action...";
+        //}
+
+        //Get: /HelloWorld/Welcome/
+        //requieres using system.text.encodings.web
+        //public string Welcome(string name, int numTimes = 1)
+        //{
+        //    return HtmlEncoder.Default.Encode($"Hello {name}, numTimes is: {numTimes}");
+        //}
+    }
+}
